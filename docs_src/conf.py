bda96f0dc23db3c140a29aa9515769f96e8df493
david clarke <clarke.davida@gmail.com>
2022-04-08 14:35:38 +0200

    trying to get documentation to look good...

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
 project = 'AnalysisToolbox'
-copyright = '2021, LatticeQCD'
+copyright = '2022, LatticeQCD'
 author = 'LatticeQCD'
 
 # The full version, including alpha/beta/rc tags
