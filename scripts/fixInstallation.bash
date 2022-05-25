f2fa534441d764a9c2996d533607969e352baf3c
david clarke <clarke.davida@gmail.com>
2022-05-25 13:10:44 +0200

    added small script to fix package installations after python upgrades

---
+++ scripts/fixInstallation.bash
@@ -0,0 +1,12 @@
+# 
+# fixInstallation.bash
+# 
+# D. Clarke 
+# 
+# It can happen that sometimes a local upgrade to Python3 will break
+# some of the packages in requirements.txt; for example an upgrade can
+# lead to a conflict between numba and numpy. This script can sometimes
+# fix these problems. 
+#
+
+pip install --upgrade --force-reinstall -r ../requirements.txt
