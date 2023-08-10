8b58679c9b64e34e32ac51c5f373f4178974209a
Jishnu Goswami <91974496+jishnuxx@users.noreply.github.com>
2023-08-10 15:42:28 +0900

    Update mainHRG.py

--- exampleapplication/mainHRG.py
+++ exampleapplication/mainHRG.py
@@ -4,7 +4,7 @@ import argparse
 from itertools import product
 from latqcdtools.base.initialize import initialize, finalize
 
-initialize('testJackknife.log')
+initialize('HRG.log')
 
 BQSC = "2000"
 
