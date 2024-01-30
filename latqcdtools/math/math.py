fitting inversions faster and more stable due to SVD; fitter speed increased; fix small bug in hessian error strat; furthermore i consider that denseObs.py bugs must be destroyed.

latqcdtools/math/math.py
@@ -9,9 +9,10 @@
 
 
 import numpy as np
-from scipy.special import poch
+import scipy as sp
 import latqcdtools.base.logger as logger
 from latqcdtools.base.utilities import isArrayLike 
+from latqcdtools.base.check import checkType
 
 
 id_2 = np.eye(2,dtype=complex)
@@ -22,6 +23,75 @@ ze_3 = np.zeros((3,3), dtype=complex)
 ze_4 = np.zeros((3,3), dtype=complex)
 
 
+def checkSquare(mat):
+    """ Make sure mat is a square np.ndarray object. 
+
+    Args:
+        mat (np.ndarray)
+    """
+    checkType(mat,np.ndarray)
+    if mat.shape[0] != mat.shape[1]:
+        logger.TBError('Expected square matrix.')
+
+
+def regulate(mat,svdcut=1e-12) -> np.ndarray:
+    """ If a matrix's singular values are too small, it will be ill-conditioned,
+    making it difficult to invert and hence reducing numerical stability. This method
+    extracts its singular values using SVD, then doctors the singular values to reduce
+    the condition number. In the context of applying an SVD cut to a covariance 
+    matrix, see e.g. Appendix D of 10.1103/PhysRevD.100.094508.
+
+    Args:
+        mat (np.ndarray)
+        svdcut (float, optional): condition number threshold. Defaults to 1e-12.
+
+    Returns:
+        np.ndarray: regulated matrix 
+    """
+    U, s, Vdagger = sp.linalg.svd(mat)
+    smax = np.max(s)
+    s[s < svdcut*smax] = svdcut*smax
+    return U @ np.diag(s) @ Vdagger 
+
+
+def invert(mat,method='scipy') -> np.ndarray:
+    """ Invert matrix.
+
+    Args:
+        mat (np.ndarray): to-be-inverted matrix 
+        method (str): algorithm for inverting the matrix
+        
+    Returns:
+        np.ndarray: mat^{-1} 
+    """
+    checkSquare(mat)
+    checkType(method,str)
+    if method=='scipy':
+        return sp.linalg.inv(mat)
+    elif method=='numpy': # Seems to be quite slow
+        return np.linalg.inv(mat)
+    elif method=='svd':
+        U, s, Vdagger = sp.linalg.svd(mat)
+        return np.conj(Vdagger.T) @ np.diag(1/s) @ np.conj(U.T)
+    elif method=='auto':
+        condition_number = np.linalg.cond(mat)
+        if condition_number > 1e12: 
+            return invert(regulate(mat,svdcut=1e-12),'svd')
+        else:
+            return invert(mat,'scipy')
+    else:
+        logger.TBError('Unrecognized inverter',method)
+
+
+def isPositiveSemidefinite(mat) -> bool:
+    eigenvalues = np.linalg.eigvals(mat)
+    return np.all(eigenvalues >= 0)
+
+
+def isSymmetric(mat) -> bool:
+    return np.allclose(mat, mat.T)
+
+
 def normalize(arr):
     return arr/np.sum(np.abs(arr))
 
@@ -30,18 +100,19 @@ def fallFactorial(n,m):
     """ Falling factorial n fall to m. """
     if m>n:
         logger.TBError("m>n.")
-    return poch(n-m+1,m)
+    return sp.special.poch(n-m+1,m)
 
 
 def riseFactorial(n,m):
     """ Rising factorial n rise to m. """
     if n>m:
         logger.TBError("n>m.")
-    return poch(n,m)
+    return sp.special.poch(n,m)
 
 
 def logDet(mat):
     """ Logarithm of determinant. """
+    checkSquare(mat)
     _, ans = np.linalg.slogdet(mat)
     return ans
 

latqcdtools/statistics/statistics.py
@@ -10,10 +10,9 @@
 
 
 import numpy as np
-from scipy.linalg import inv
-from scipy.special import betainc, erf
+import scipy as sp
 from latqcdtools.math.num_deriv import diff_jac 
-from latqcdtools.math.math import logDet, normalize
+from latqcdtools.math.math import logDet, normalize, invert, isPositiveSemidefinite, isSymmetric
 from latqcdtools.base.plotting import fill_param_dict, plot_fill, plot_lines
 from latqcdtools.base.utilities import isHigherDimensional, toNumpy
 from latqcdtools.base.cleanData import clipRange
@@ -197,8 +196,9 @@ def DOF(ndat,nparam,priorsigma=None) -> int:
     return dof
 
 
-def chisquare(xdata,ydata,cov,func,args=(),params=(),prior=None,priorsigma=None):
-    """ Calculate chi^2.
+def chisquare(xdata,ydata,cov,func,args=(),params=(),prior=None,priorsigma=None) -> float:
+    """ Calculate chi^2, see e.g. eq. (8.28) of Sivia and Skilling or eq. (A1) of
+    10.1103/PhysRevD.90.054506. We assume priors are not correlated with data.
 
     Args:
         xdata (array-like)
@@ -215,15 +215,14 @@ def chisquare(xdata,ydata,cov,func,args=(),params=(),prior=None,priorsigma=None)
     """
     checkPrior(prior,priorsigma)
     y    = expandArgs(func,xdata,params,args)
-    cor  = norm_cov(cov)
-    diff = ( ydata - y )/np.sqrt( np.diag(cov) )
-    res  = diff.dot( inv(cor).dot(diff) )
+    diff = ydata-y
+    res  = diff @ invert(cov) @ diff
     if prior is not None:
         res += np.sum((np.array(params) - prior)**2 / priorsigma**2)
     return res
 
 
-def logGBF(xdata, ydata, cov, func, args=(), params=(), prior=None, priorsigma=None):
+def logGBF(xdata, ydata, cov, func, args=(), params=(), prior=None, priorsigma=None) -> float:
     """ log P(data|model). This quantity is useful for comparing fits of the same data to different models that
     have different priors and/or fit functions. The model with the largest logGBF is the one preferred by the data.
     Differences in logGBF smaller than 1 are not very significant. Gaussian statistics are assumed.
@@ -244,17 +243,13 @@ def logGBF(xdata, ydata, cov, func, args=(), params=(), prior=None, priorsigma=N
     chi2   = chisquare(xdata, ydata, cov, func, args, params, prior, priorsigma)
     nparam = countParams(func,params)
     dof    = DOF(len(ydata),nparam,priorsigma)
-    logger.debug('chi^2 =',chi2)
-    logger.debug('nparam =',nparam)
-    logger.debug('dof =',dof)
-    logger.debug('priorsigma =',priorsigma)
     if prior is None:
         return 0.5*( - logDet(cov) - chi2 - dof*np.log(2*np.pi) )
     else:
         return 0.5*( - logDet(cov) - chi2 - dof*np.log(2*np.pi) + logDet(np.diag(clipRange(priorsigma)**2)) )
 
 
-def AIC(xdata, ydata, cov, func, args=(), params=(), prior=None, priorsigma=None):
+def AIC(xdata, ydata, cov, func, args=(), params=(), prior=None, priorsigma=None) -> float:
     """ The Akaike information criterion (AIC) is a measure of how well a fit performs. It builds on the likelihood
     function by including a penalty for each d.o.f. This is useful in a context where you have multiple models to
     choose from,and hence different numbers of d.o.f. possible. It's also useful when you are worried about
@@ -278,7 +273,7 @@ def AIC(xdata, ydata, cov, func, args=(), params=(), prior=None, priorsigma=None
     return 2*nparam - 2*likelihood
 
 
-def AICc(xdata, ydata, cov, func, args=(), params=(), prior=None, priorsigma=None):
+def AICc(xdata, ydata, cov, func, args=(), params=(), prior=None, priorsigma=None) -> float:
     """ Corrected AIC (AICc). When the sample size is smaller, it increases the chance AIC will select a model with too
     many parameters. The AICc tries to further correct for this. In the limit that the number of data points goes to
     infinity, one recovers the AIC.
@@ -296,14 +291,14 @@ def AICc(xdata, ydata, cov, func, args=(), params=(), prior=None, priorsigma=Non
     Returns:
         float: corrected AIC 
     """
-    nparam  = countParams(func,params)
-    nprior  = countPriors(priorsigma) 
-    ndat    = len(ydata) + nprior
-    aic     = AIC(xdata, ydata, cov, func, args, params, prior, priorsigma)
+    nparam = countParams(func,params)
+    nprior = countPriors(priorsigma) 
+    ndat   = len(ydata) + nprior
+    aic    = AIC(xdata, ydata, cov, func, args, params, prior, priorsigma)
     return aic + 2*(nparam**2+nparam)/(ndat-nparam+1)
 
 
-def BAIC(xdata, ydata, cov, func, args=(), params=(), Ncut=0, modelPrior=1):
+def BAIC(xdata, ydata, cov, func, args=(), params=(), Ncut=0, modelPrior=1) -> float:
     """ Bayesian Akaike information criterion of 2208.14983.
 
     Args:
@@ -361,19 +356,27 @@ def weighted_mean(data, err) -> float:
 def weighted_variance(err) -> float:
     """ Get variance of above weighted mean, when the weights are statistical errors. 
 
-    Parameters
-    ----------
+    Args:
+        err (array-like)
 
-    err: array_like
-        The errors of the data points.
+    Returns:
+        float: weighted variance 
     """
     weights = 1/np.array(err)**2
     return 1/np.sum(weights)
 
 
 def biased_sample_variance(data, err) -> float:
-    """ Compute the biased weighted sample variance, i.e. the biased variance of an individual measurement and not the
-    variance of the mean. """
+    """ Compute the biased weighted sample variance, i.e. the biased variance of an 
+    individual measurement and not the variance of the mean.
+
+    Args:
+        data (array-like)
+        err (array-like)
+
+    Returns:
+        float: sample variance 
+    """
     mean = weighted_mean(data, err)
     weights = 1/np.array(err)**2
     V1 = np.sum(weights)
@@ -400,27 +403,17 @@ def unbiased_mean_variance(data, err) -> float:
     return biased_sample_variance(data, weights) * V2 / ( V1**2 - V2)
 
 
-def norm_cov(cov) -> np.ndarray:
-    """ Normalize a covariance matrix to create the correlation matrix. """
-    res = np.zeros((len(cov), len(cov[0])))
-    for i in range(len(cov)):
-        for j in range(len(cov[0])):
-            res[i][j] = cov[i][j] / np.sqrt( cov[j][j] * cov[i][i] )
-    return np.array(res)
+def cov_to_cor(cov) -> np.ndarray:
+    """ Normalize covariance matrix to create correlation matrix.
 
+    Args:
+        cov (np.ndarray)
 
-def cut_eig(corr, threshold):
-    """ Cut eigenvalues of the correlation matrix. If they are smaller than the threshold, replace them with the
-    threshold. When needed, this replaces a small eigenvalue by a larger, small eigenvalue, which has the effect of
-    slightly overestimating the errors. The alternative would be to ignore them, in which case the program would
-    crash because the matrix is singular, or to discard them, which is like setting the variance to infinity.
-    This procedure is more accurate than the latter option. """
-    vals, vecs = np.linalg.eig(corr)
-    for i, value in enumerate(vals):
-        if value < threshold:
-            logger.details('Set small eigenvalue',value,'from correlation matrix to threshold',threshold)
-            vals[i] = threshold
-    return vecs.dot( np.diag(vals).dot( vecs.transpose() ) )
+    Returns:
+        np.ndarray: correlation matrix 
+    """
+    diagonal_sqrt = np.sqrt(np.diag(cov))
+    return cov / np.outer(diagonal_sqrt, diagonal_sqrt)
 
 
 @reduce_tuple
@@ -444,8 +437,8 @@ def dev_by_dist(data, axis=0, return_both_q=False, percentile=68):
 def error_prop(func, means, errors, grad=None, args=()):
     """ Use error propagation to propagate some errors through function func. The function should have the form
         func( data ), where data is your array of input variables. """
-    errors = np.asarray(errors)
-    means  = np.asarray(means)
+    errors = np.array(errors)
+    means  = np.array(means)
     mean   = func(means, *args)
 
     # Test if we got a covariance matrix
@@ -458,7 +451,8 @@ def error_prop(func, means, errors, grad=None, args=()):
     if grad is not None:
         grad = grad(means, *args)
     else:
-        grad = diff_jac(means, func, args).transpose()
+        grad = diff_jac(means, func, args).T
+
     error = 0
     try:
         for i in range(len(grad)):
@@ -516,7 +510,7 @@ def gaudif(x1,e1,x2,e2) -> float:
         logger.TBError('Error bars should be non-negative. Got',e1,e2)
     sigma = np.sqrt(e1**2 + e2**2)
     z     = abs(x1-x2)/(sigma * np.sqrt(2.))
-    return 1.0 - erf(z)
+    return 1.0 - sp.special.erf(z)
 
 
 def studif(x1,e1,ndat1,x2,e2,ndat2) -> float:
@@ -548,9 +542,9 @@ def studif(x1,e1,ndat1,x2,e2,ndat2) -> float:
     if t==0:
         return 1
     elif t>0:
-        return betainc(dof/2,1/2,x)
+        return sp.special.betainc(dof/2,1/2,x)
     else:
-        return 2-betainc(dof/2,1/2,x)
+        return 2-sp.special.betainc(dof/2,1/2,x)
 
 
 def plot_func(func, domain, params=(), args=(), func_err=None, params_err=(), 
@@ -568,9 +562,9 @@ def plot_func(func, domain, params=(), args=(), func_err=None, params_err=(),
         swapXY (bool, optional): Swap X and Y variables in plot. Defaults to False.
         npoints (int, optional): Number of points to use for plotting. Defaults to 1000.
     """
+    checkDomain(domain)
     fill_param_dict(kwargs)
     kwargs['marker'] = None
-    checkDomain(domain)
     xmin = domain[0] 
     xmax = domain[1] 
 
@@ -604,7 +598,7 @@ def plot_func(func, domain, params=(), args=(), func_err=None, params_err=(),
             return plot_lines(xdata, ydata, yedata=None, xedata=None, **kwargs)
 
 
-def getModelWeights(IC):
+def getModelWeights(IC) -> np.ndarray:
     """ Convert information criteria IC to normalized probability weights.
 
     Args:

testing/math/testMath.py
@@ -5,14 +5,19 @@
 # 
 # For testing some of the basic math functions.
 #
+import numpy as np
 import math
 import latqcdtools.base.logger as logger
-from latqcdtools.math.math import fallFactorial 
+from latqcdtools.math.math import fallFactorial, riseFactorial, invert 
 from latqcdtools.testing import print_results, concludeTest
 
 
 logger.set_log_level('INFO')
 
+mat = [[ 1,2,-1],
+       [ 2,1, 2],
+       [-1,2, 1]]
+mat = np.array(mat)
 
 def testMath():
 
@@ -20,6 +25,11 @@ def testMath():
 
     lpass *= print_results(fallFactorial(23,23),1.*math.factorial(23),text='fallFactorial 23!')
     lpass *= print_results(fallFactorial(6,3),6*5*4,text='fallFactorial 6_fall_3')
+
+    inv = invert(mat,'scipy')
+
+    lpass *= print_results(inv,invert(mat,'numpy'),text='scipy vs numpy')
+    lpass *= print_results(inv,invert(mat,'svd'),text='scipy vs svd')
 
     concludeTest(lpass)
 

testing/statistics/testFit.py
@@ -30,9 +30,6 @@ def fit_func(x, p):
 def grad_fit_func(x, p):
     return np.array([x**2, x, np.ones(len(x))])
 
-def hess_fit_func(x, p):
-    return np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-
 
 ''' Correlator fit. '''
 def one_state(x, p, Nt):
@@ -43,10 +40,6 @@ def grad_one_state(x, p, Nt):
     A, m = p
     return np.array([np.cosh(m * (Nt / 2 - x)), A * np.sinh(m * (Nt / 2 - x)) * (Nt / 2 - x)])
 
-def hess_one_state(x, p, Nt):
-    A, m = p
-    return np.array([ [0, np.sinh(m * (Nt / 2 - x)) * (Nt / 2 - x)],
-                      [np.sinh(m*(Nt/2-x))*(Nt/2-x), A*np.cosh(m*(Nt/2-x)) * (Nt/2-x)**2] ])
 
 def calc_cov_OLD(data):
     data = np.asarray(data)
@@ -124,6 +117,11 @@ def testFit():
     lpass *= print_results(res, res_true, res_err, res_err_true, "Exact TNC",prec=EPSILON)
 
 
+    res, res_err, _ = do_fit(fit_func, xdata, ydata, edata, [1, 1, 1], algorithm="TNC", grad = grad_fit_func, 
+                             norm_err_chi2=True,error_strat='hessian')
+    lpass *= print_results(res, res_true, res_err, res_err_true, "Exact TNC, hessian error strat",prec=EPSILON)
+
+
     res, res_err, _ = do_fit(fit_func, xdata, ydata, edata, [1, 1, 1], algorithm="L-BFGS-B", derive_chisq = True, norm_err_chi2=True)
     lpass *= print_results(res, res_true, res_err, res_err_true,"Numerical L-BFGS-B using built-in derivative",prec=EPSILON)
 
@@ -147,19 +145,18 @@ def testFit():
     res_err_true = [5.042611e-08, 8.380914e-05]
 
 
-    res, res_err, _ = do_fit(one_state, xdata, ydata, edata, [1, 1], grad=grad_one_state, hess=hess_one_state,
-                                     args=(64,), norm_err_chi2=True, algorithm="curve_fit")
+    res, res_err, _ = do_fit(one_state, xdata, ydata, edata, [1, 1], grad=grad_one_state,
+                             args=(64,), norm_err_chi2=True, algorithm="curve_fit")
     lpass *= print_results(res, res_true, res_err, res_err_true, "Exact curve_fit",prec=EPSILON)
 
 
     res, res_err, _ = do_fit(one_state, xdata, ydata, np.diag(edata)**2, [1, 1], grad=grad_one_state,
-                                     hess=hess_one_state, args=(64,), norm_err_chi2=True,
-                                     algorithm="curve_fit")
+                             args=(64,), norm_err_chi2=True, algorithm="curve_fit")
     lpass *= print_results(res, res_true, res_err, res_err_true, "Diagonal correlation matrix",prec=EPSILON)
 
 
     res, res_err, _ = do_fit(one_state, xdata, ydata, edata, [1, 1], args=(64,), use_diff = False,
-                                     norm_err_chi2=True, algorithm="curve_fit")
+                             norm_err_chi2=True, algorithm="curve_fit")
     lpass *= print_results(res, res_true, res_err, res_err_true, "Numerical curve_fit with difference quotient applied on chisquare",
                            prec=EPSILON)
 
@@ -167,7 +164,7 @@ def testFit():
     # Numerical derivative gives a slightly different result
     res_err_true = [5.0425819803e-08, 8.38114689761e-05]
     res, res_err, _ = do_fit(one_state, xdata, ydata, edata, [1, 1], args=(64,), use_diff = True,
-                                     norm_err_chi2=True, algorithm="curve_fit", )
+                             norm_err_chi2=True, algorithm="curve_fit")
     lpass *= print_results(res, res_true, res_err, res_err_true,"Numerical curve_fit with difference quotient",prec=EPSILON)
 
 
@@ -188,7 +185,7 @@ def testFit():
         lpass = False
 
     res, res_err, _ = do_fit(one_state, xdata, ydata, cov / nconfs, res_true, grad=grad_one_state,
-                             hess=hess_one_state, args=(64,), norm_err_chi2=True,algorithm="curve_fit")
+                             args=(64,), norm_err_chi2=True,algorithm="curve_fit")
     res_true = [4.988713e-05, 2.950030e-01]
     res_err_true = [1.176005e-06, 5.573209e-04]
     lpass *= print_results(res, res_true, res_err, res_err_true, "Exact curve_fit for correlated data",prec=EPSILON)

testing/statistics/testStats.py
@@ -7,7 +7,7 @@
 #
 
 import numpy as np
-from latqcdtools.statistics.statistics import gaudif, studif, pearson, std_mean
+from latqcdtools.statistics.statistics import gaudif, studif, pearson, std_mean, cov_to_cor 
 import latqcdtools.base.logger as logger
 from latqcdtools.testing import print_results, concludeTest
 
@@ -87,6 +87,43 @@ ts2 = [-0.41765878, -0.2333498 ,  -0.22939907,  -0.27035445,  -0.29163003,  -0.2
         0.5776071 ,  0.6235754 ,   0.67287165,   0.82511437,   0.93292713,   0.84517425,
         0.762654  ,  0.8910726 ,   0.9227938 ,   0.7618559 ,   0.80128413]
 
+# An example taken from P. Lepage's lsqfit tutorial:
+ycov  = np.array(
+       [[ 2.1537808808e-09,   8.8161794696e-10,   3.6237356558e-10,
+          1.4921344875e-10,   6.1492842463e-11,   2.5353714617e-11,
+          4.3137593878e-12,   7.3465498888e-13],
+       [  8.8161794696e-10,   3.6193461816e-10,   1.4921610813e-10,
+          6.1633547703e-11,   2.5481570082e-11,   1.0540958082e-11,
+          1.8059692534e-12,   3.0985581496e-13],
+       [  3.6237356558e-10,   1.4921610813e-10,   6.1710468826e-11,
+          2.5572230776e-11,   1.0608148954e-11,   4.4036448945e-12,
+          7.6008881270e-13,   1.3146405310e-13],
+       [  1.4921344875e-10,   6.1633547703e-11,   2.5572230776e-11,
+          1.0632830128e-11,   4.4264622187e-12,   1.8443245513e-12,
+          3.2087725578e-13,   5.5986403288e-14],
+       [  6.1492842463e-11,   2.5481570082e-11,   1.0608148954e-11,
+          4.4264622187e-12,   1.8496194125e-12,   7.7369196122e-13,
+          1.3576009069e-13,   2.3914810594e-14],
+       [  2.5353714617e-11,   1.0540958082e-11,   4.4036448945e-12,
+          1.8443245513e-12,   7.7369196122e-13,   3.2498644263e-13,
+          5.7551104112e-14,   1.0244738582e-14],
+       [  4.3137593878e-12,   1.8059692534e-12,   7.6008881270e-13,
+          3.2087725578e-13,   1.3576009069e-13,   5.7551104112e-14,
+          1.0403917951e-14,   1.8976295583e-15],
+       [  7.3465498888e-13,   3.0985581496e-13,   1.3146405310e-13,
+          5.5986403288e-14,   2.3914810594e-14,   1.0244738582e-14,
+          1.8976295583e-15,   3.5672355835e-16]]
+       )
+
+
+def norm_cov(cov) -> np.ndarray:
+    """ A more transparent covariance normalizer. """ 
+    res = np.zeros((len(cov), len(cov[0])))
+    for i in range(len(cov)):
+        for j in range(len(cov[0])):
+            res[i][j] = cov[i][j] / np.sqrt( cov[j][j] * cov[i][i] )
+    return np.array(res)
+
 
 def testStats():
 
@@ -126,6 +163,10 @@ def testStats():
     xbar1 = std_mean(ts1)
     xbar2 = std_mean(ts2)
     R = np.sum((ts1-xbar1)*(ts2-xbar2))/np.sqrt(np.sum((ts1-xbar1)**2)*np.sum((ts2-xbar2)**2))
+
+    # Two simple tests of method normalizing a covariance matrix.
+    lpass *= print_results(cov_to_cor(ycov),cov_to_cor(cov_to_cor(ycov)),text='cov_to_cor fixed point')
+    lpass *= print_results(cov_to_cor(ycov),norm_cov(ycov),text='cov_to_cor')
 
     lpass *= print_results(pearson(ts1,ts2),R)
 