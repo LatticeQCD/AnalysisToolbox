296b766d39fdef51e5c8482c311c629bec3005b6
david clarke <clarke.davida@gmail.com>
2024-05-26 18:56:48 -0600

    working toward pip install

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read_requirements():
+    """Read requirements.txt and return a list of dependencies."""
+    with open("requirements.txt", "r") as fh:
+        return fh.read().splitlines()
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="AnalysisToolbox",
+    version="1.2.0",
+    author="D. A. Clarke",
+    author_email="clarke.davida@gmail.com",
+    description="A set of Python tools for statistically analyzing correlated data. This includes aspects of lattice QCD applications related to QCD phenomenology.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LatticeQCD/AnalysisToolbox",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=read_requirements(),
+)
